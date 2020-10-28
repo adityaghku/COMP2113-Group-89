6de481a873ac88e05f3fc566df3b1dd281f266b0
Aditya Gupta <aditya@Adityas-MacBook-Pro.local>
2020-10-28 18:50:27 +0800

    Main skeleton structure, will updated

--- main.cpp
+++ main.cpp
@@ -1,8 +1,35 @@
 #include <iostream>
 using namespace std;
 
-
 int main(){
-  
+
+  bool isTrue=true;
+  int purse;
+  cout << "Load game or new" << endl;
+  //Load Game or New Game
+
+  //Initialise the Cash, name, and key
+
+  //cout << "Instructions???";
+
+  //Do you want to start game?
+
+  while(istrue){
+    //bet amount
+    //shuffledeck()
+    //Deal cards - when the dealer and player arrays get updated
+    //Output(cash,dealer array, your array)
+    //blackjack condition
+    //User(&cash,&hit) //User move options - while loop
+    //Dealer move options
+    //Determine winner of game
+    //Update bet amount of the player
+    //Check condition: No money or quit
+    //update users.txt
+  }
+
+  //If no money
+  //cout << "Key"
+
   return 0;
 }
