40d24dc53e6b7a96e8971426920397edc951da97
Aditya Gupta <aditya@Adityas-MacBook-Pro.local>
2020-11-17 23:24:14 +0800

    FUCK

--- main.cpp
+++ main.cpp
@@ -1,38 +1,64 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
 using namespace std;
+
+//class.cpp
+class playerData{
+  public:
+    string name;
+    int wallet;
+    string key;
+
+    int wins;
+    int gamesPlayed; //Stats
+    int winPercentage;
+};
+
+//printboard.cpp
+int numberofDigits(int n);
+void printdealerCards(int n);
+void printplayerCards(int n);
+void PrintBoard(int wallet,int currentbet, string playerCards[15], string dealerCards[15], int playercards, int dealercards);
+
+//instructions
+void displayInstructions();
 
 int main(){
 
   bool istrue=true,didyouwin=false;
-  int purse,currentbet,cardnumber=0;
+  int wallet,currentbet,cardnumber=0;
 
   playerData P = { };
 
-  str playerCards[15],dealerCards[15],deck[52];
+  str playerCards[15], dealerCards[15], deck[52];
 
-  cout << "Load game or New?" << endl; //Load Game or New Game
-  string choice;
-  cin >> choice;
+//------------------------------LOAD GAME FUNCTIONALITY START--------------------------------------------
 
-  if (choice == "Load"){
-    cout << "Please enter your key: " << endl;
-    string inputkey;
-    cin >> inputkey;
-    loadGame(inputkey);
-  }
+  //cout << "Load game or New?" << endl; //Load Game or New Game
+  //string choice;
+  //cin >> choice;
 
-  else{
-    cout << "What is your name?" << endl;
-    cin >> name; //Initialise the Cash, name, and key
+  //if (choice == "Load"){
+    //cout << "Please enter your key: " << endl;
+    //string inputkey;
+    //cin >> inputkey;
+    //loadGame(inputkey);
+  //}
 
-    generateKey(&key) //Make key only alphabets for simplicity
+  //else{
+    //cout << "What is your name?" << endl;
+    //cin >> name; //Initialise the Cash, name, and key
 
-    cout << "How much money do you want to buy-in?" << endl;
-    cin >> purse;
+    //generateKey(&key) //Make key only alphabets for simplicity
 
-    playerData playerdata[i] = {name, purse, key};
-  }
+  //  cout << "How much money do you want to buy-in?" << endl;
+  //  cin >> wallet;
+
+  //  playerData playerdata[i] = {name, wallet, key};
+//  }
+
+//------------------------------LOAD GAME FUNCTIONALITY END--------------------------------------------
 
   cout << "Do you want to view the instructions? Y or N" << endl; //Instructions
   char instructions;
@@ -47,17 +73,17 @@ int main(){
   while(istrue){
     cout << "How much do you want to bet (in an increment of 10)" << endl; //bet amount
     cin >> currentbet;
-    purse-=currentbet; //for user board display
+    wallet-=currentbet; //for user board display
 
     shuffleDeck(&deck); //shuffles the deck
     dealCards(deck,&playerCards,&dealerCards,&cardnumber); //Deals the cards to the player and the dealer
 
-    PrintBoard(purse,currentbet,playerCards,dealerCards); //Prints table for user visual appeal
+    PrintBoard(wallet,currentbet,playerCards,dealerCards); //Prints table for user visual appeal
 
     if blackjack(playerCards){ //checking if the player has blackjack
-      purse+=2.5*currentbet;
+      wallet+=2.5*currentbet;
       cout << "You have blackjack" << endl;
-      PrintBoard(purse,currentbet,playerCards,dealerCards);
+      PrintBoard(wallet,currentbet,playerCards,dealerCards);
       didyouwin=true;
     }
     else{
@@ -81,19 +107,19 @@ int main(){
     }
 
     if(didyouwin){ //Update bet amount of the player
-      purse+=2*currentbet
+      wallet+=2*currentbet
       cout << "You win" << endl;
-      PrintBoard(purse,currentbet,playerCards,dealerCards);
+      PrintBoard(wallet,currentbet,playerCards,dealerCards);
     }
 
     else{
       cout << "You lose" << endl;
-      PrintBoard(purse,currentbet,playerCards,dealerCards);
+      PrintBoard(wallet,currentbet,playerCards,dealerCards);
     }
 
     updateUsers(playerdata); //update users.txt
 
-    if(purse==0){
+    if(wallet==0){
       cout << "You have no more money left, you lose" << endl; //Money check condition
       istrue=false;
     }
@@ -106,9 +132,156 @@ int main(){
     }
   }
 
-  if(purse!=0){
+  if(wallet!=0){
     cout << playerdata.key << " is the key";
   }
 
   return 0;
+}
+
+//---------------PRINTBOARD START------------------------------------------
+int numberofDigits(int n){
+  int count = 0;
+  while (n != 0){
+    n = n / 10;
+    ++count;
+  }
+  return count;
+}
+
+void printdealerCards(int n){
+  int l = 34 - 7*(n-1);
+
+  for (int i = 0; i < n; i++){
+    if (i == 0){
+      cout << "|" << setw(35) << " ---- " << " ";
+    }
+    else{
+      cout << " ---- " << " ";
+    }
+  }
+  cout << setw(l) << "|" << endl;
+
+  for (int i = 0; i < n; i++){
+    if (i == 0){
+      cout << "|" << setw(30) << "|" << dealerCards[i] << "   |" << " ";
+    }
+    else{
+      cout << "|" << dealerCards[i] << "   |" << " ";
+    }
+  }
+  cout << setw(l) << "|" << endl;
+
+  for (int i = 0; i < n; i++){
+    if (i == 0){
+      cout << "|" << setw(35) << "|    |" << " ";
+    }
+    else{
+      cout << "|    |" << " ";
+    }
+  }
+  cout << setw(l) << "|" << endl;
+
+  for (int i = 0; i < n; i++){
+    if (i == 0){
+      cout << "|" << setw(30) << "|" << "   " << dealerCards[i] << "|" << " ";
+    }
+    else{
+      cout << "|" << "   " << dealerCards[i] << "|" << " ";
+    }
+  }
+  cout << setw(l) << "|" << endl;
+
+  for (int i = 0; i < n; i++){
+    if (i == 0){
+      cout << "|" << setw(35) << " ---- " << " ";
+    }
+    else{
+      cout << " ---- " << " ";
+    }
+  }
+  cout << setw(l) << "|" << endl;
+}
+
+void printplayerCards(int n){
+  int l = 34 - 7*(n-1);
+
+  for (int i = 0; i < n; i++){
+    if (i == 0){
+      cout << "|" << setw(35) << " ---- " << " ";
+    }
+    else{
+      cout << " ---- " << " ";
+    }
+  }
+  cout << setw(l) << "|" << endl;
+
+  for (int i = 0; i < n; i++){
+    if (i == 0){
+      cout << "|" << setw(30) << "|" << playerCards[i] << "   |" << " ";
+    }
+    else{
+      cout << "|" << playerCards[i] << "   |" << " ";
+    }
+  }
+  cout << setw(l) << "|" << endl;
+
+  for (int i = 0; i < n; i++){
+    if (i == 0){
+      cout << "|" << setw(35) << "|    |" << " ";
+    }
+    else{
+      cout << "|    |" << " ";
+    }
+  }
+  cout << setw(l) << "|" << endl;
+
+  for (int i = 0; i < n; i++){
+    if (i == 0){
+      cout << "|" << setw(30) << "|" << "   " << playerCards[i] << "|" << " ";
+    }
+    else{
+      cout << "|" << "   " << playerCards[i] << "|" << " ";
+    }
+  }
+  cout << setw(l) << "|" << endl;
+
+  for (int i = 0; i < n; i++){
+    if (i == 0){
+      cout << "|" << setw(35) << " ---- " << " ";
+    }
+    else{
+      cout << " ---- " << " ";
+    }
+  }
+  cout << setw(l) << "|" << endl;
+}
+
+void PrintBoard(int wallet,int currentbet, string playerCards[15], string dealerCards[15], int playercards, int dealercards){
+  cout << setfill('-') << setw(72) << " " << endl;
+  cout << setfill(' ');
+
+  cout << "|" << setw(70) << "|" << endl;
+  printdealerCards(dealercards);
+  cout << "|" << setw(70) << "|" << endl;
+  cout << "|" << setw(70) << "|" << endl;
+  cout << "|" << setw(70) << "|" << endl;
+  cout << "|" << "      wallet: " << "$" << wallet;
+  int l = 70 - 14 - numberofDigits(wallet);
+  cout << setw(l) << "|" << endl;
+  cout << "|" << setw(70) << "|" << endl;
+  cout << "|" << "      Bet Amount: " << "$" << currentbet;
+  l = 70 - 19 - numberofDigits(currentbet);
+  cout << setw(l) << "|" << endl;
+  printplayerCards(playercards);
+  cout << "|" << setw(70) << "|" << endl;
+  cout << "|" << setw(70) << "|" << endl;
+  cout << "|" << setw(70) << "|" << endl;
+
+  cout << setfill('-') << setw(72) << " " << endl;
+}
+//---------------PRINTBOARD END------------------------------------------
+
+void displayInstructions(){
+  cout << "Instructions will be printed here" << endl;
 }
